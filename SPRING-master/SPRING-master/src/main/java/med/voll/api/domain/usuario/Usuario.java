0c06931b8ff897656c2c88056a4b9f42177e9e2f
wictor costa <142526733+uictor3@users.noreply.github.com>
2024-09-17 19:47:29 -0300

    Add files via upload
    pode pegar amanda, eu deixo.

---
+++ SPRING-master/SPRING-master/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String senha;
+}
